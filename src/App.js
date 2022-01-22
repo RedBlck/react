8f9ae765eb142238587133f7332b2d7e5d2629ac
RedBlck <pentsakav@gmail.com>
2022-01-22 21:46:15 +0200

    hw

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="container">
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
