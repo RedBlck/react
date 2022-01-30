109a6bafe30b9a2dc1587357ea14323ac9049e4d
RedBlck <pentsakav@gmail.com>
2022-01-31 01:11:26 +0200

    vers0.9

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import {useEffect, useState} from "react";
+
 import './App.css';
+import Form from "./components/Form/Form";
+import Users from "./components/Users/Users";
+import {userService} from "./components/services/user.service";
 
 function App() {
+    const [users, setUsers] = useState([]);
+    const [findUsers, setFindUsers] = useState([]);
+
+    useEffect(()=>{
+        userService.getAll()
+            .then(value => {
+                setUsers([...value])
+                setFindUsers([...value])
+            })
+    },[])
+
+    const getFilter = (data) => {
+      let arr = [...users]
+
+        if (data.name) {
+            arr = arr.filter(user => user.name.toLowerCase().includes(data.name.toLowerCase()));
+        }
+        if (data.username) {
+            arr = arr.filter(user => user.username.toLowerCase().includes(data.username.toLowerCase()));
+        }
+        if (data.email) {
+            arr = arr.filter(user => user.email.toLowerCase().includes(data.email.toLowerCase()));
+        }
+        setFindUsers(arr)
+    }
   return (
     <div className="App">
-
+        <Form getFilter={getFilter}/>
+        <Users users={findUsers}/>
     </div>
   );
 }
